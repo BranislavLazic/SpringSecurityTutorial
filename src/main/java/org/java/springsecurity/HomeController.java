23095961d55058dc6046ff49e538afff2841a34c
BranislavLazic <brano2411@hotmail.com>
2014-10-22 11:47:38 +0200

    4th tutorial

--- src/main/java/org/java/springsecurity/HomeController.java
+++ src/main/java/org/java/springsecurity/HomeController.java
@@ -1,5 +1,7 @@
 package org.java.springsecurity;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,8 +10,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HomeController {
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home() {
-
+	public String home(HttpServletRequest request) {
+		System.out.println(request.getRemoteAddr());
 		return "home";
 	}
 
