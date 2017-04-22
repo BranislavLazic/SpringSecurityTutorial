a420ac04adb770b637a174ae94a98ec53fb869fc
Branislav Lazic <branislav.lazic@codecentric.de>
2017-04-22 18:18:30 +0200

    Add DAO configuration

--- src/main/java/org/java/springsecurity/HomeController.java
+++ src/main/java/org/java/springsecurity/HomeController.java
@@ -1,7 +1,5 @@
 package org.java.springsecurity;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,8 +8,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HomeController {
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(HttpServletRequest request) {
-		System.out.println(request.getRemoteAddr());
+	public String home() {
 		return "home";
 	}
 
